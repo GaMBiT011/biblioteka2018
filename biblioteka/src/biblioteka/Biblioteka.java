e1ff8484505925d5dca86542e14b8184f28cb31f
GaMBiT011 <nikola.madic97@gmail.com>
2018-03-09 10:51:42 +0100

    Logicka kontrola 2 za Biblioteku

--- biblioteka/src/biblioteka/Biblioteka.java
+++ biblioteka/src/biblioteka/Biblioteka.java
@@ -4,16 +4,15 @@ import java.util.LinkedList;
 
 import biblioteka.interfejs.BibliotekaInterfejs;
 
-public class Biblioteka	implements BibliotekaInterfejs {
-	private LinkedList<Knjiga> knjige=new LinkedList<>();
-	
+public class Biblioteka implements BibliotekaInterfejs {
+	private LinkedList<Knjiga> knjige = new LinkedList<>();
+
 	public void dodajKnjigu(Knjiga k) {
 		knjige.add(k);
 	}
 
-
 	public void obrisiKnjigu(Knjiga k) {
-		knjige.remove(k);	
+		knjige.remove(k);
 	}
 
 	@Override
@@ -23,13 +22,16 @@ public class Biblioteka	implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if (naslov == null)
+			return null;
+
 		LinkedList<Knjiga> novaLista = new LinkedList<>();
 
-			for (int i = 0; i < knjige.size(); i++) {
-				if(knjige.get(i).getNaslov().contains(naslov))
-						novaLista.add(knjige.get(i));
+		for (int i = 0; i < knjige.size(); i++) {
+			if (knjige.get(i).getNaslov().contains(naslov))
+				novaLista.add(knjige.get(i));
 		}
 		return novaLista;
 	}
-	
+
 }
