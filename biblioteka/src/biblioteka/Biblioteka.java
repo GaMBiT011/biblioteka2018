63a6d62d52b9761bd6482016275d3b2325f4bc41
GaMBiT011 <nikola.madic97@gmail.com>
2018-03-09 11:07:26 +0100

    Revert "dodat komentar"
    This reverts commit 221824ae2ec76a6a46a91d4239d7e7ee3b2f8210.

--- biblioteka/src/biblioteka/Biblioteka.java
+++ biblioteka/src/biblioteka/Biblioteka.java
@@ -28,7 +28,6 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
-		// ovo je okey komentar
 		if (naslov == null)
 			throw new RuntimeException("Mora te uneti naslov.");
 
